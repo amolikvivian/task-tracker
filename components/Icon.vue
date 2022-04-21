ee35b19af974135677d4bf53380ee4e114ac195d
amolikvivian <amolikvivian@gmail.com>
2022-04-22 04:55:19 +0530

    Added: Icons

--- components/Icon.vue
+++ components/Icon.vue
@@ -93,6 +93,22 @@
         d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
       />
     </svg>
+
+    <svg
+      v-if="name === 'arrow-left'"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      stroke-width="2"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+      />
+    </svg>
   </div>
 </template>
 
