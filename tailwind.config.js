d337c27163ed10c0cdb30ef007966744f333955c
amolikvivian <amolikvivian@gmail.com>
2022-04-22 08:23:01 +0530

    Fixed: Dynamic Class issue, added safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  theme: {},
-  plugins: [],
   content: [
     `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
     `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
     `App.{js,ts,vue}`,
     `app.{js,ts,vue}`,
   ],
+  purge: {
+    options: {
+      // List your classes here, or you can even use RegExp
+      safelist: [/^bg-/, /^text-/],
+    },
+  },
 }
