52ba6432f80bb3d4ab15847d1acdf69a37fa1a32
amolikvivian <amolikvivian@gmail.com>
2022-04-22 10:41:09 +0530

    Added: Cancel and Save icons

--- components/Icon.vue
+++ components/Icon.vue
@@ -72,7 +72,7 @@
     <svg
       v-if="name === 'delete'"
       xmlns="http://www.w3.org/2000/svg"
-      class="h-4 w-4"
+      class="h-4 w-4 text-red-600"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
@@ -110,7 +110,7 @@
       xmlns="http://www.w3.org/2000/svg"
       class="h-6 w-6"
       fill="none"
-      viewBox="0 0 24 24"
+      viewBox="0 0 12 12"
       stroke="currentColor"
       stroke-width="1"
     >
@@ -123,7 +123,7 @@
     <svg
       v-if="name === 'plus-circle'"
       xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
+      class="h-5 w-5"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
@@ -133,6 +133,36 @@
         stroke-linecap="round"
         stroke-linejoin="round"
         d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+    <svg
+      v-if="name === 'check'"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-5 w-5 text-gray-700"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      stroke-width="1"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+      />
+    </svg>
+    <svg
+      v-if="name === 'cross'"
+      xmlns="http://www.w3.org/2000/svg"
+       class="h-5 w-5 text-red-600"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      stroke-width="1"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
       />
     </svg>
   </div>
